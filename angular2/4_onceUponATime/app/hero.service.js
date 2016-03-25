86db07688dc906d54a0efb045d897be5ef19e362
Frode Klevstul <signup@klevstul.com>
2016-03-25 22:35:12 +0100

    completed part 5 https://angular.io/docs/ts/latest/tutorial/toh-pt5.html

--- angular2/4_onceUponATime/app/hero.service.js
+++ angular2/4_onceUponATime/app/hero.service.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './mock-heroes'], function(exports_1, context_1) {
+System.register(['./mock-heroes', 'angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,15 +10,15 @@ System.register(['angular2/core', './mock-heroes'], function(exports_1, context_
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, mock_heroes_1;
+    var mock_heroes_1, core_1;
     var HeroService;
     return {
         setters:[
-            function (core_1_1) {
-                core_1 = core_1_1;
-            },
             function (mock_heroes_1_1) {
                 mock_heroes_1 = mock_heroes_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
             }],
         execute: function() {
             HeroService = (function () {
@@ -34,6 +34,9 @@ System.register(['angular2/core', './mock-heroes'], function(exports_1, context_
                     } // 2 seconds
                      // 2 seconds
                     );
+                };
+                HeroService.prototype.getHero = function (id) {
+                    return Promise.resolve(mock_heroes_1.HEROES).then(function (heroes) { return heroes.filter(function (hero) { return hero.id === id; })[0]; });
                 };
                 HeroService = __decorate([
                     core_1.Injectable(), 
